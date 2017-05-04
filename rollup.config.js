c220ce221f29540b7023e9241e0bdf6ae68aa971
Craig Morton <CraigMorton22@gmail.com>
2017-05-04 12:45:04 +0100

    Add coffeescript plugin to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
+import coffeescript from 'rollup-plugin-coffee-script'
+
 export default {
   entry: 'src/linebreaker.coffee',
   dest: 'dist/linebreaker.js',
   moduleName: 'linebreak',
   format: 'es',
-  plugins: []
+  plugins: [
+    coffeescript(),
+  ]
 }
