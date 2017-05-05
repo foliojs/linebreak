27a8bb591c4b350c65d890d4e9befe0536152f6b
Craig Morton <CraigMorton22@gmail.com>
2017-05-05 15:36:13 +0100

    Add umd output format version of rollup config

---
+++ rollup.umd.config.js
@@ -0,0 +1,21 @@
+import coffeescript from 'rollup-plugin-coffee-script'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import browserifyPlugin from 'rollup-plugin-browserify-transform'
+import brfsBabel from 'brfs-babel'
+
+export default {
+  entry: 'src/linebreaker.coffee',
+  dest: 'dist/linebreaker.umd.js',
+  moduleName: 'linebreak',
+  format: 'umd',
+  plugins: [
+    coffeescript(),
+    nodeResolve({ extensions: ['.js', '.coffee'] }),
+    commonjs({
+      extensions: [ '.js', '.coffee' ],
+      sourceMap: false,
+    }),
+    browserifyPlugin(brfsBabel, {exclude: 'node_modules/**'}),
+  ]
+}
